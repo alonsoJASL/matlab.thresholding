a860427fb084754b6b6e94adb00cfe8920ca1489
Jose Alonso Solis Lemus <alonso.jasl@gmail.com>
2016-10-26 12:58:39 +0100

    Complex edge detection algorithm added.

---
+++ complexedge.m
@@ -0,0 +1,21 @@
+function [BW, edgehandles] = complexedge(dataIn)
+% COMPLEX EDGE DETECTION.
+% Does not return a binary image. 
+%
+% usage: [BW, edgeshandles] = complexedge(Xg)
+% 
+[Xcanny] = edge(dataIn,'Canny', multithresh(dataIn,2));
+[Xacanny] = edge(dataIn,'approxcanny', multithresh(dataIn,2));
+[Xprewitt] = edge(dataIn,'Prewitt');
+[Xlog] = edge(dataIn,'log');
+[Xrob] = edge(dataIn,'Roberts');
+
+BW = mean(cat(3,Xcanny,Xacanny, Xprewitt,Xlog,Xrob),3);
+
+if nargout > 1
+    edgehandles.canny = Xcanny;
+    edgehandles.approxcanny = Xacanny;
+    edgehandles.prewitt = Xprewitt;
+    edgehandles.log = Xlog;
+    edgehandles.roberts = Xrob;
+end
